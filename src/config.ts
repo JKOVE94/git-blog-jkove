c4f072acff2f9338b2c79e1644e322406f5ca43a
Daehyeon Kim <jkove1994@gmail.com>
2025-06-28 09:56:39 +0900

    Initial commit

---
+++ src/config.ts
@@ -0,0 +1,71 @@
+import type {
+	ExpressiveCodeConfig,
+	LicenseConfig,
+	NavBarConfig,
+	ProfileConfig,
+	SiteConfig,
+} from "./types/config";
+import { LinkPreset } from "./types/config";
+
+export const siteConfig: SiteConfig = {
+	title: "jkove's Blog",
+	subtitle: "개발 일지",
+	lang: "ko",
+	themeColor: {
+		hue: 250,
+		fixed: false,
+	},
+	banner: {
+		enable: false,
+		src: "assets/images/demo-banner.png",
+		position: "center",
+		credit: {
+			enable: false,
+			text: "",
+			url: "",
+		},
+	},
+	toc: {
+		enable: true,
+		depth: 2,
+	},
+	favicon: [],
+};
+
+export const navBarConfig: NavBarConfig = {
+	links: [
+		LinkPreset.Home,
+		LinkPreset.Archive,
+		LinkPreset.About,
+		{
+			name: "GitHub",
+			url: "https://github.com/jkove",
+			external: true,
+		},
+	],
+};
+
+export const profileConfig: ProfileConfig = {
+	avatar: "assets/images/demo-avatar.png",
+	name: "jkove",
+	bio: "개발자의 기술 블로그",
+	links: [
+		{
+			name: "GitHub",
+			icon: "fa6-brands:github",
+			url: "https://github.com/jkove",
+		},
+	],
+};
+
+export const licenseConfig: LicenseConfig = {
+	enable: true,
+	name: "CC BY-NC-SA 4.0",
+	url: "https://creativecommons.org/licenses/by-nc-sa/4.0/",
+};
+
+export const expressiveCodeConfig: ExpressiveCodeConfig = {
+	// Note: Some styles (such as background color) are being overridden, see the astro.config.mjs file.
+	// Please select a dark theme, as this blog theme currently only supports dark background color
+	theme: "github-dark",
+};
